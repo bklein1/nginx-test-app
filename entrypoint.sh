dc017717d525591828e93145d90ef27ed8831b1d
Bastian Klein <bastianklein92@gmail.com>
2017-06-25 16:14:42 +0200

    Initial working commit

---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+sed -e "s|\${NGINX_PATH_APP_1}|${NGINX_PATH_APP_1}|g" \
+    -e "s|\${FLASK_APP_1_URL}|${FLASK_APP_1_URL}|g" \
+    -e "s|\${NGINX_PATH_APP_2}|${NGINX_PATH_APP_2}|g" \
+    -e "s|\${FLASK_APP_2_URL}|${FLASK_APP_2_URL}|g" \
+    /etc/nginx/conf.d/nginx.conf.tpl > /etc/nginx/conf.d/nginx.conf || exit 1
+
+
+if [ "$DEBUG" = "true" ]; then
+  echo "NGINX_PATH_APP_1    : ${NGINX_PATH_APP_1}"
+  echo "FLASK_APP_1_URL     : ${FLASK_APP_1_URL}"
+  echo "NGINX_PATH_APP_2    : ${NGINX_PATH_APP_2}"
+  echo "FLASK_APP_2_URL     : ${FLASK_APP_2_URL}"
+  echo
+
+  echo "--------------- 8< /etc/nginx/conf.d/nginx.conf >8 ---------------"
+  cat /etc/nginx/conf.d/nginx.conf
+  echo "--------------- 8< /etc/nginx/conf.d/nginx.conf >8 ---------------"
+fi
+
+if [ -z "$1" ]; then
+    echo '/usr/sbin/nginx -g "daemon off;"'
+    /usr/sbin/nginx -g "daemon off;"
+else
+    echo "/usr/sbin/nginx $@"
+    /usr/sbin/nginx "$@"
+fi
